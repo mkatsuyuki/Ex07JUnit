95c3b3b1656353097e0b634d0d83e68ee5042e86
Paulo Katsuyuki Muraishi Kamimura <paulo_katsuyuki@hotmail.com>
2020-05-22 16:21:28 -0300

    Deletando classes inputeis

--- src/Professor.java
+++
@@ -1,9 +0,0 @@
-public class Professor {
-    private String nome;
-    private int nUSP;
-
-    public Professor(String nome, int nUSP){
-        this.nome = nome;
-        this.nUSP = nUSP;
-    }
-}
